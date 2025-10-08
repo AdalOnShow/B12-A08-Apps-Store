8f4d2b3aeed43a3e30493b12dfe2a058c7d0fa52
Sharif H. Adal <mdadalbadsha2008@gmail.com>
2025-10-08 12:03:54 +0600

    Add SmallCard component and utility function for number formatting; update TrendingApp to display app data

--- pages/home/TrendingApp.jsx
+++ pages/home/TrendingApp.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router"
+import SmallCard from "../../components/SmallCard"
 import useAppData from "../../hooks/useAppData"
 
 const TrendingApp = () => {
-  const [appData, loading, error] = useAppData()
+  const [appData] = useAppData()
 
-  const eigthData = appData.slice(0, 8)
+  const eighthData = appData.slice(0, 8)
 
 
   return (
@@ -11,8 +13,16 @@ const TrendingApp = () => {
       <h2 className="text-col-pry text-5xl font-bold text-center">Trending Apps</h2>
       <p className="text-[#627382] mt-4 text-xl text-center">Explore All Trending Apps on the Market developed by us</p>
 
-      <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        {}
+      <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {eighthData.map(appData => (
+          <SmallCard key={appData.id} appData={appData} />
+        ))}
+      </div>
+
+      <div className="flex-center">
+        <Link to="/apps" className="btn px-10 !py-[15px] bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white font-semibold">
+          <span>Show All</span>
+        </Link>
       </div>
     </div>
   )
